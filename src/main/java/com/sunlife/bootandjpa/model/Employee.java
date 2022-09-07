7c6e80c959a86dfe6ae4ba5e3fdeb7430e803a1d
Raghav-Github1989 <58589896+Raghav-Github1989@users.noreply.github.com>
2022-09-07 23:01:18 +0530

    Add files via upload
    Employee.java Updated

--- src/main/java/com/sunlife/bootandjpa/model/Employee.java
+++ src/main/java/com/sunlife/bootandjpa/model/Employee.java
@@ -1,1 +1,66 @@
+package com.sunlife.bootandjpa.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column
+	private int empid;
+	@Column
+	private String name;
+	@Column
+	private int age;
+	@Column
+	private String city;
+	
+	public Employee() {}
+	
+	public Employee(int empid, String name, int age, String city) {
+		super();
+		this.empid = empid;
+		this.name = name;
+		this.age = age;
+		this.city = city;
+	}
+
+	public int getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
