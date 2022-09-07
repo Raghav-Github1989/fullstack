d73e52258b39d7a298f1a5464c7a9f2014e8c799
Raghav-Github1989 <58589896+Raghav-Github1989@users.noreply.github.com>
2022-09-07 22:58:48 +0530

    Add files via upload
    EmployeeRepository.java

--- src/main/java/com/sunlife/bootandjpa/dao/EmployeeRepository.java
+++ src/main/java/com/sunlife/bootandjpa/dao/EmployeeRepository.java
@@ -1,1 +1,9 @@
+package com.sunlife.bootandjpa.dao;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.sunlife.bootandjpa.model.Employee;
+
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer>{
+}
