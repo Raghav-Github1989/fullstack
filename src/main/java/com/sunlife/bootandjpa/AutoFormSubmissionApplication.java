ee25d16544e28910c5bb72500d975d65e680661d
Raghav-Github1989 <58589896+Raghav-Github1989@users.noreply.github.com>
2022-09-07 22:54:53 +0530

    Added Main java files
    Added Main java files

---
+++ src/main/java/com/sunlife/bootandjpa/AutoFormSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.sunlife.bootandjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFormSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoFormSubmissionApplication.class, args);
+	}
+
+}
--- src/main/java/com/sunlife/bootandjpa/ServletInitializer.java
+++ src/main/java/com/sunlife/bootandjpa/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BootandjpaApplication.class);
+		return application.sources(AutoFormSubmissionApplication.class);
 	}
 
 }
