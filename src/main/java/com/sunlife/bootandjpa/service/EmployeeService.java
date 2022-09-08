60483220875862096bd60b96f78f9357dfc86ba8
Raghav-Github1989 <58589896+Raghav-Github1989@users.noreply.github.com>
2022-09-08 08:30:09 +0530

    updated EmployeeService.java
    Updated EmployeeService.java

--- src/main/java/com/sunlife/bootandjpa/service/EmployeeService.java
+++ src/main/java/com/sunlife/bootandjpa/service/EmployeeService.java
@@ -1,1 +1,43 @@
+package com.sunlife.bootandjpa.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sunlife.bootandjpa.dao.EmployeeRepository;
+import com.sunlife.bootandjpa.model.Employee;
+
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	public int addEmployee(Employee employee) {
+		employeeRepository.save(employee);
+		return employee.getEmpid();
+	}
+	
+	public void updateEmployee(Employee employee) {
+		employeeRepository.save(employee);
+	}
+	
+	public Employee getEmployee(int empId) {
+		return employeeRepository.findById(empId).get();
+	}
+
+	public List<Employee> getAllEmployee() {
+		List<Employee> employees = new ArrayList<>();
+		employeeRepository.findAll().forEach(e -> employees.add(e));
+		return employees;
+	}
+
+	public int deleteEmployee(int empId) {
+		employeeRepository.deleteById(empId);
+		return empId;
+	}
+
+
+}
